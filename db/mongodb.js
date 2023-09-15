2cb84116a2dc3e7be87e5e145cd3c1778b8607c7
Julian Tobito <julianmontoua@gmail.com>
2023-09-14 22:33:46 -0500

    mi segundo commit

---
+++ db/mongodb.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connection.on('open', () => console.log('db Connected'))
+
+async function connectDb({ host, port, dbName }) {
+
+    const uri = `mongodb://${host}:${port}/${dbName}`
+    mongoose.connect(uri, { useNewUrlParser: true })
+}
+
+module.exports = connectDb
